4eef9e8898004f77906d807ce93f4c9d985eb1db
Gilbert Torchon <gilbert@noukod.com>
2019-01-29 19:21:58 +0000

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,4 +4,12 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
+  def index
+    if params[:author_id]
+      @posts = Author.find(params[:author_id]).posts
+    else
+      @posts = Post.all
+    end
+  end
+
 end
