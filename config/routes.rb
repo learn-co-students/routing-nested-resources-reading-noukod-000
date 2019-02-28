d6ca7e323fa2320fe5f972753aaf27086b9f7d81
Merline Belizaire <merline@noukod.com>
2019-02-28 17:14:04 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :authors do
-  resources :posts do
-    resources :comments
-  end
-end
-# @post = Post.find(params[:id])
-# @post.author
+  resources :authors, only: [:show] do
+   # nested resource for posts
+   resources :posts, only: [:show, :index]
+ end
 
+ resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+
+ root 'posts#index'
 end
