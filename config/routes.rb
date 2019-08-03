904a268b3431fc79559523e64c568c81d01f04c0
RickyyCoder <ricardopaul459@gmail.com>
2019-08-03 10:14:50 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  #params[:author_id]
+  #params[:author_id] #pp
   #once the user navigates to that #authors#show rails provides the #author_id key
   resources :authors, only: [:show] do 
     resources :posts, only: [:show, :index]
